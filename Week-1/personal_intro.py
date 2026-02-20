17ed65b23c3c1415d6ba0eabc7250b5ab30638ab
prajjwal-github <singhprajjwal845@gmail.com>
2026-02-20 09:05:54 +0530

    Add Week 1 tasks

---
+++ Week-1/personal_intro.py
@@ -0,0 +1,7 @@
+print("Welcome! Let's get to know you.")
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+hobby = input("What is your hobby? ")
+
+print(f"Hello {name}, you are {age} years old and you like {hobby}.")
